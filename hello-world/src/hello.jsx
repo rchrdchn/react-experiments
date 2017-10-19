7f3757bc525018e09a9f31ae99c3bdd43123032f
Richard Chan <richardchanme@gmail.com>
2017-10-19 15:41:37 +0200

    exp hello world components

---
+++ hello-world/src/hello.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Hello extends Component {
+	render() {
+		const hello = "hello";
+		return (
+			<div>
+			{hello}
+			</div>
+		);
+	}
+}
+
+export default Hello;
---
+++ hello-world/src/world.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class World extends Component {
+	render() {
+		const world = "world";
+		return (
+			<div>
+			{world}
+			</div>
+		);
+	}
+}
+
+export default World;
