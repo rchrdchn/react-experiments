d68aa2aa4216bf2c1bb2f4b909ed9202140921d8
Richard Chan <richardchanme@gmail.com>
2018-01-31 23:02:20 -0800

    Added Material UI to resume project

--- resume/src/App.js
+++ resume/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+import { Navbar, Jumbotron, Button } from 'react-bootstrap';
 
 const PERSONAL = [
     {
