ca5b1ddd182cc7c6a94435e95e0f549386a07390
Richard Chan <richardchanme@gmail.com>
2018-02-05 23:51:11 -0800

    Added initial structure for scoreboard

--- scoreboard/src/App.js
+++ scoreboard/src/App.js
@@ -4,11 +4,25 @@ import { Grid, Jumbotron } from 'react-bootstrap';
 class App extends Component {
   render() {
     return (
-      <div>
-      <Jumbotron>
-        <h1>Hello, world!</h1>
-        <p>Text</p>
-      </Jumbotron>
+      <div className="scoreboard">
+        <div className="header">
+            <h1>Scoreboard</h1>
+        </div>
+
+        <div className="players">
+            <div className="player">
+                <div className="player-name">
+                    Richard
+                </div>
+                <div className="player-score">
+                    <div className="counter">
+                        <button className="counter-action decrement"> - </button>
+                        <div className="counter-score"> 31 </div>
+                        <button className="counter-action increment"> + </button>
+                    </div>
+                </div>
+            </div>
+        </div>
       </div>
     );
   }
